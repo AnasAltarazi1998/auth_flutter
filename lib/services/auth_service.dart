f7a00dc5012297f87780cfd7ed486be2da847c93
anasa <anasa@techno-visions.net>
2022-05-12 18:10:37 +0300

    fix logical bug in the service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,14 @@
 class AuthService {
-  bool login({required String email, required String password}) {
-    bool result = false;
-    result = email.contains('@');
-    result = email.endsWith('.com');
-    result = email.length > 7;
-    result = password.length > 8;
+  Future<bool> login({required String email, required String password}) async {
+    bool result = true;
+    result = result && email.contains('@');
+    result = result && email.endsWith('.com');
+    result = result && email.length > 7;
+    result = result && password.length > 8;
+    result = await Future.delayed(
+      const Duration(seconds: 3),
+      () => result,
+    );
     return result;
   }
 }
